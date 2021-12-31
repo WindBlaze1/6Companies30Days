297dc4e13c9d53ceb0d6da1044e6b30b353eb15b
Harit <53652715+WindBlaze1@users.noreply.github.com>
2021-12-31 20:38:06 +0530

    Update Q7.cpp

--- GoldmanSachs/Q7.cpp
+++ GoldmanSachs/Q7.cpp
@@ -1,1 +1,36 @@
 
+// { Driver Code Starts
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        if(N == 1)
+            return N;
+        int k = (M%N + K - 1)%(N);
+        if(k==0)
+            return N;
+        return k;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
