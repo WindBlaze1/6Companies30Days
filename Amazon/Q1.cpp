8410e27b487a15f3547deeacef7f58f17d904d23
Harit <53652715+WindBlaze1@users.noreply.github.com>
2022-01-10 22:03:04 +0530

    Update Q1.cpp

--- Amazon/Q1.cpp
+++ Amazon/Q1.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+ public:
+   int maxProfit(int k, int n, int a[]) {
+       int dp[k+1][n];
+       memset(dp,0,sizeof(dp));
+       for(int i=1;i<=k;i++)
+       {
+           int mx=INT_MIN;
+           for(int j=1;j<n;j++)
+           {
+               mx=max(mx,dp[i-1][j-1]-a[j-1]);
+               dp[i][j]=max(dp[i][j-1],mx+a[j]);
+           }
+       }
+       return dp[k][n-1];
+   }
+};
