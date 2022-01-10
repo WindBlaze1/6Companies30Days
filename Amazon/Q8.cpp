57eb1c2a4164a9257a54dbb5887c1c4e87777318
Harit <53652715+WindBlaze1@users.noreply.github.com>
2022-01-10 21:46:24 +0530

    Create Q8.cpp

---
+++ Amazon/Q8.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        return 1+m/2;
+    }
+};
