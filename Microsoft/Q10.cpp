a583dd59bdacfd61ca4c9e51eae92e3b396bbe36
Harit <53652715+WindBlaze1@users.noreply.github.com>
2022-01-11 16:57:03 +0530

    Create Q10.cpp

---
+++ Microsoft/Q10.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        int r1=arr[0],r2=0;
+        for(int i=2; i<=n; i++){
+            int t = max(r2+arr[i-1],r1);
+            r2 = r1;
+            r1 = t;
+        }
+        return r1;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
