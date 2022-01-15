a8c20d3de0c195eeae0c2bbf728550712d627a2f
Harit <53652715+WindBlaze1@users.noreply.github.com>
2022-01-15 20:10:40 +0530

    Update Q2.cpp

--- Microsoft/Q2.cpp
+++ Microsoft/Q2.cpp
@@ -1,1 +1,41 @@
-
+class Solution {
+public:
+    
+    bool iscycle(int src, vector<bool>& visited, vector<bool>& recstack, vector<int> adj[]){
+        if(visited[src] == false){
+            visited[src] = true;
+            recstack[src] = true;
+            for(auto it : adj[src]){
+                if(!visited[it] and iscycle(it,visited,recstack,adj)){
+                    return true;
+                }
+                else{
+                    if(recstack[it]){
+                        return true;
+                    }
+                }
+            }
+        }
+        recstack[src] = false;
+        return false;
+    }
+	bool isPossible(int N, vector<pair<int, int> >& pre) {
+	    // Code here
+	   // create a directed graph of all the edges
+	    vector<int>adj[N];
+	    for(int i=0; i<pre.size(); i++){
+	        int u = pre[i].first, v = pre[i].second;
+	        adj[u].push_back(v);
+	    }
+	    vector<bool>visited(N,false);
+	    vector<bool>recstack(N,false);
+	    for(int i=0; i<N; i++){
+	        if(visited[i] == false){
+	            if(iscycle(i,visited,recstack,adj)){
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+};
