81948ed363755b918e6aa5dae0da59a789fc7df9
Harit <53652715+WindBlaze1@users.noreply.github.com>
2022-01-15 20:15:39 +0530

    Update Q5.cpp

--- Microsoft/Q5.cpp
+++ Microsoft/Q5.cpp
@@ -1,3 +1,12 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+
 class Solution
 {
    public:
@@ -29,3 +38,32 @@ class Solution
       return ans;
    }
 };
+
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
