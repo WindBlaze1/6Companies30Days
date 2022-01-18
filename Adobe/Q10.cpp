32055e4e8824c228bb8efdd91e44ba9578a8fab1
Harit <53652715+WindBlaze1@users.noreply.github.com>
2022-01-18 19:39:06 +0530

    Create Q10.cpp

---
+++ Adobe/Q10.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <cstdlib>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+  public:
+  
+    //Function to return the name of candidate that received maximum votes.
+    vector<string> winner(string arr[],int n)
+    {
+        // Your code here
+        // Return the string containing the name and an integer
+        // representing the number of votes the winning candidate got
+        map<string,int>mp;
+        int maxV(0);
+        for(int i=0; i<n; i++){
+            mp[arr[i]]++;
+            maxV = max(maxV,mp[arr[i]]);
+        }
+        for(auto &i:mp){
+            if(i.second == maxV){
+                return {i.first,to_string(i.second)};
+            }
+        }
+        return {};
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    for(int i=0;i<t;i++)
+    {
+        
+        
+        int n;
+        cin>>n;
+        
+        string arr[n];
+        
+        for (int i=0;i<n;i++)
+        cin>>arr[i];
+        Solution obj;
+        vector<string> result = obj.winner(arr,n);
+        
+        cout<<result[0] << " " << result[1] << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
